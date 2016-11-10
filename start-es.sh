b61cb67ac032dd0be740a920458f08b15d8ad829
Hardik Patel <hardikgw@gmail.com>
2016-11-10 04:13:01 -0500

    conigs

--- start-es.sh
+++ start-es.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run  --name es -it -v $(PWD)/esconfig:/es/config -v $(PWD)/esdata:/es/data -p 9200:9200 -p 9300:9300 channelit/elastic-node
+docker run  --name es -it -v $(PWD)/esconfig:/es/config -v $(PWD)/esdata:/es/data -v $(PWD)/eslogs:/es/log -p 9200:9200 -p 9300:9300 channelit/elastic-node
