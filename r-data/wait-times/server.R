a6b06573ce0e48beef24a501ae5535971c407a2f
Hardik Patel <hardikgw@gmail.com>
2016-11-13 11:45:06 -0500

    reactive

--- r-data/wait-times/server.R
+++ r-data/wait-times/server.R
@@ -9,10 +9,11 @@ airports <- read.csv(file="WaitTimesPerDay.csv", header=TRUE, sep=","
 airports$Date <- as.Date(airports$Date, format="%m/%d/%y")
 
 shinyServer(function(input, output) {
-  subset <- subset(airports, as.Date(Date) >= as.Date("2016-08-01") & as.Date(Date) <= as.Date("2016-08-01"))
   output$airports <- renderLeaflet({
+    subset <- subset(airports, as.Date(Date) >= as.Date(input$date_range[1]) & as.Date(Date) <= as.Date(input$date_range[2]))
+    
     leaflet(subset) %>% addTiles() %>%
       addCircles(lng = ~Lon, lat = ~Lat, weight = 1,
-                 radius = ~Count * 20, popup = ~Airport)
+                 radius = ~Count/2 * 20, popup = ~Airport)
   })
 })
